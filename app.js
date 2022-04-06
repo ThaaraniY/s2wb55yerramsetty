ea63a5128829d02f6994fdc21e94604d0fb2378e
ThaaraniY <98137201+ThaaraniY@users.noreply.github.com>
2022-04-06 16:36:19 -0500

    Updated the selector class

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var honeyRouter = require('./routes/honey'); 
+var addmobRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector'); 
 
 var app = express();
 
@@ -22,7 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/honey', honeyRouter)
+app.use('/honey', honeyRouter);
+app.use('/addmods', addmobRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
